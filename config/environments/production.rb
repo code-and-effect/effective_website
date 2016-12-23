295342a4a57459be59bf79153562fd5a1b6a2986
Matt Riemer <matthew@agilestyle.com>
2016-12-23 09:51:12 -0700

    Document no trailing slash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Email
-  config.action_mailer.default_url_options = { host: 'https://www.example.com' }
-  config.action_mailer.asset_host = 'https://www.example.com'
+  config.action_mailer.default_url_options = { host: 'https://www.example.com' }  # No trailing /
+  config.action_mailer.asset_host = 'https://www.example.com' # No trailing /
 
   # ExceptionNotification gem
   config.middleware.use ExceptionNotification::Rack,
