3790d21a4b70d3c63209a5b91b8926ddd217a080
Matt Riemer <matthew@agilestyle.com>
2018-02-15 16:12:43 -0700

    forms

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,7 +27,7 @@ class Ability
 
     can :index, Effective::Log
     can :show, Effective::Page do |page| page.roles_permit?(user) end
-    can :show, Effective::StyleGuide
+    can [:index, :show], Effective::StyleGuide
     can :manage, Effective::Trash, user_id: user.id
 
     if user.is?(:admin)
