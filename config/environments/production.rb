542fda5b978d5b7e81320adaed48b7226b7a9b7f
Matt Riemer <matthew@codeandeffect.com>
2018-07-26 11:00:30 -0600

    turn on force_ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.force_ssl = true
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
