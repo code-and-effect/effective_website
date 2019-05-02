c09b6d3b989a3f9a5d02ef2d5379a5df585e332d
Matt Riemer <matthew@codeandeffect.com>
2019-05-02 12:19:18 -0600

    User avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,12 @@ module ApplicationHelper
   def user_tag(user, name: true)
     user ||= User.new
 
-    avatar = if user.avatar_attached? && user.avatar.attached? && (url = url_for(user.avatar) rescue false)
-      content_tag(:span, class: 'user-avatar', title: user.to_s) do
-        image_tag(url, alt: user.to_s)
-      end
-    else
-      content_tag(:span, user.to_s, title: user.to_s)
+    avatar_tag = if user.avatar_attached? && user.avatar.attached? && (url = url_for(user.avatar) rescue false)
+      content_tag(:span, class: 'user-avatar', title: user.to_s) { image_tag(url, alt: user.to_s) }
     end
 
-    name ? (avatar + ' ' + content_tag(:span, user.to_s, class: 'user-name')) : avatar
+    name_tag = content_tag(:span, user.to_s, class: 'user-name')
+
+    [(avatar_tag if avatar_tag.present?), (name_tag if avatar_tag.blank? || name)].compact.join(' ').html_safe
   end
 end
