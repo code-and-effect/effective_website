97b01fff1f680f9615fc8fc0521e1844a4727038
Matt Riemer <matthew@codeandeffect.com>
2019-12-31 15:09:22 -0700

    bundle update

--- db/migrate/20180227174349_create_active_storage_tables.active_storage.rb
+++ db/migrate/20180227174349_create_active_storage_tables.active_storage.rb
@@ -22,5 +22,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
 
       t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
     end
+
+    add_foreign_key :active_storage_attachments, :active_storage_blobs, column: :blob_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_165346) do
+ActiveRecord::Schema.define(version: 2019_04_20_034056) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
