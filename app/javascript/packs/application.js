88293aae567c726b298e912dab2ef02fccf79539
Matt Riemer <matthew@codeandeffect.com>
2020-09-08 12:23:03 -0600

    yarn add stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,25 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+// CSS
+import 'stylesheets/application'
 
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// Rails Javascripts
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
 
-console.log('Hello World from Webpacker')
+// jQuery
+// We assign window.jQuery so that the asset pipeline (effective_gems) can use it
+import jQuery from 'jquery'
+window.jQuery = window.$ = jQuery;
+
+// Bootstrap
+import 'popper.js'
+import 'bootstrap'
+
+// ActionCable
+// import 'channels'
+
+// Stimulus and StimulusReflex
+// import 'controllers'
